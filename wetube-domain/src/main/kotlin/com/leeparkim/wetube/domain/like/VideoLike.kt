98d21f747cb39b869a94426b9473aa2eda0b028b
Jaemin.Park <once29@naver.com>
2022-11-12 16:06:46 +0900

    Update VideoLike

--- wetube-domain/src/main/kotlin/com/leeparkim/wetube/domain/like/VideoLike.kt
+++ wetube-domain/src/main/kotlin/com/leeparkim/wetube/domain/like/VideoLike.kt
@@ -1,5 +1,6 @@
 package com.leeparkim.wetube.domain.like
 
+import com.leeparkim.wetube.domain.BaseEntity
 import com.leeparkim.wetube.domain.user.User
 import com.leeparkim.wetube.domain.video.Video
 import javax.persistence.Entity
@@ -12,4 +13,4 @@ class VideoLike(
     val user: User,
     @ManyToOne(fetch = FetchType.LAZY)
     val video: Video,
-)
+) : BaseEntity()
