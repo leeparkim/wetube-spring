6572894ef887a9272821f4ead4ea3c46f4074437
eunseo9808 <eunseo9808@naver.com>
2022-12-18 00:19:17 +0900

    feat: Take care of common exception

--- wetube-api/src/main/kotlin/com/leeparkim/wetube/presentation/APIResponse.kt
+++ wetube-api/src/main/kotlin/com/leeparkim/wetube/presentation/APIResponse.kt
@@ -1,4 +1,23 @@
 package com.leeparkim.wetube.presentation
 
-class APIResponse {
+import com.leeparkim.wetube.domain.ResultCode
+
+data class ApiResponse<T>(
+        val code: String,
+        val message: String,
+        val data: T? = null
+) {
+    companion object {
+        fun <T> empty(): ApiResponse<T> {
+            return ApiResponse("", "", null)
+        }
+
+        fun <T> success() = ApiResponse<T>(ResultCode.SUCCESS.name, ResultCode.SUCCESS.message, null)
+
+        fun <T> success(data: T) = ApiResponse(ResultCode.SUCCESS.name, ResultCode.SUCCESS.message, data)
+
+        fun <T> failure(code: String, message: String) = ApiResponse<T>(code, message, null)
+
+        fun <T> failure(resultCode: ResultCode) = ApiResponse<T>(resultCode.name, resultCode.message, null)
+    }
 }
--- wetube-domain/src/main/kotlin/com/leeparkim/wetube/domain/ResultCode.kt
+++ wetube-domain/src/main/kotlin/com/leeparkim/wetube/domain/ResultCode.kt
@@ -1,4 +1,16 @@
 package com.leeparkim.wetube.domain
 
-class ResultCode {
+enum class ResultCode(val message: String = "") {
+    SUCCESS("성공"),
+
+    // authorization
+    UNAUTHORIZED("인증 실패"),
+    FORBIDDEN("권한 없음"),
+
+    // 기타
+    BAD_REQUEST("요청에 오류가 있습니다"),
+    INTERNAL_SERVER_ERROR("서버 에러"),
+
+    // User
+    USER_NOT_FOUND("유저 조회 실패"),
 }
