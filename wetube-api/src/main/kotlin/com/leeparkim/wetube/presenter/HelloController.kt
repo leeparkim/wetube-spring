17c9c16fd952ed1a14e1aebdd00462618a270a3a
Jaemin.Park <once29@naver.com>
2023-01-04 00:24:33 +0900

    Cealnup codes

--- wetube-api/src/main/kotlin/com/leeparkim/wetube/presenter/HelloController.kt
+++ wetube-api/src/main/kotlin/com/leeparkim/wetube/presenter/HelloController.kt
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
     @GetMapping("/hello")
-    fun hello(): String = "Hello, acm-api"
+    fun hello(): String = "Hello, wetube-api"
 }
